27c5e1ced1160f2d9563408182084463a05e026b
roy murray <80788182+RoyalRoys435@users.noreply.github.com>
2022-11-07 08:28:15 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+class Main {
+  public static void main(String[] args) {
+    //type casting data types
+
+    System.out.println(" float to int: ");
+    float r = 3.99F;
+    int i = (int)r;
+    System.out.println(i);
+
+    System.out.println("int to float");
+    r = i;
+    System.out.println(r);
+
+    int x = 4;
+    float z= 3.58F;
+    System.out.println(x + z);
+
+    char a ='A';
+    System.out.println((int)a);
+
+    char b = 'Z';
+    System.out.println((int)b);
+  }
+}
